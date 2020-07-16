97970d250dd61833e6fac2739e8f68f53ab6b1b8
Alisson Lima <alissonpcl@outlook.com>
2020-07-16 20:46:16 -0300

    Adicionado frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { MyButton } from "components";
+import "./App.css";
+function App() {
+  return (
+    <div className="App">
+      <MyButton>Hello world</MyButton>
+    </div>
+  );
+}
+export default App;
